a266d4459714a75a963351e47ac97d1f81807fca
ThanaphatKalaya <ohmmee1@gmail.com>
2017-04-02 01:32:30 +0700

    re-cammit ref code

--- server.js
+++ server.js
@@ -45,11 +45,11 @@ app.get('/index', function(req, res) {
 });
 
 app.get('/search', function(req, res) {
-  var query = 'SELECT cat_desc FROM catagories';
+  var query = 'SELECT cat_name FROM catagories';
   connection.query(query, function (error, results, fields) {
     // catagories =
-    // [ RowDataPacket { cat_desc: 'Catagory 1' },
-    // RowDataPacket { cat_desc: 'Catagory 2' } ]
+    // [ RowDataPacket { cat_name: 'Catagory 1' },
+    // RowDataPacket { cat_name: 'Catagory 2' } ]
     res.render('pages/search', {
       pagename: 'search',
       pageheader: 'Search',
@@ -59,39 +59,36 @@ app.get('/search', function(req, res) {
 });
 
 app.post('/search', function(req, res) {
-  var cat_id = req.body.cat_id;
-  res.redirect('/catagory/' + cat_id);
-});
-
-app.get('/catagory/:cat_id', function(req, res) {
-  var cat_id = req.params["cat_id"];
+  var cat_name = req.body.cat_name;
+  console.log(req.body.cat_name);
   var query = 'SELECT item.i_id, item.i_name, item.i_price, item.i_unit, images.img_link, catagories.cat_name from item ';
   query += 'INNER JOIN images ON item.i_id = images.i_id ';
   query += 'INNER JOIN item_cat ON item.i_id = item_cat.i_id ';
-  query += 'INNER JOIN catagories ON item_cat.i_id = catagories.i_id ';
-  query += 'WHERE catagories.cat_id = ' + cat_id;
+  query += 'INNER JOIN catagories ON item_cat.cat_id = catagories.cat_id ';
+  query += 'WHERE catagories.cat_name = "' + cat_name + '" ';
   query += 'GROUP BY item.i_id LIMIT 20';
+  console.log(query);
   connection.query(query, function (error, results, fields) {
     res.render('pages/index', {
-      pagename: 'index',
-      pageheader: 'Catagory: ' + results[0][cat_name],
+      pagename: 'Catagory: ' + cat_name,
+      pageheader: 'Catagory: ' + cat_name,
       item_table: results
     });
   });
 });
 
-// app.get('/item/:i_id', function(req, res) {
-//   var i_id = req.params["i_id"];
-//   var query = '';
-//   query += 'SELECT ';
-//   connection.query(query, function (error, results, fields) {
-//     res.render('pages/search', {
-//       pagename: 'search'
+app.get('/item/:i_id', function(req, res) {
+  var i_id = req.params["i_id"];
+  var query = '';
+  query += 'SELECT ';
+  connection.query(query, function (error, results, fields) {
+    res.render('pages/search', {
+      pagename: 'search'
 
-//     });
-// 
-//   });
-// });
+    });
+
+  });
+});
 
 var port = 8081
 app.listen(port);
